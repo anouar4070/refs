9b0ba41bda1f3b113baccba2079a80c82565aa1a
anouar garbaa <g.anouar@yahoo.com>
2024-10-02 16:03:45 +0100

    using ref to set & clean timer in TimerChallenge component

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,16 +1,40 @@
+import { useState, useRef } from "react";
+
+// let timer;
 
 export default function TimerChallenge({ title, targetTime }) {
+  // every component instance of TimerChallenge component will get its own timer ref that works totally independent from othe refs that belong to othr instances of tha component.
+const timer = useRef();
+
+  const [timerStarted, setTimerStarted] = useState(false);
+  const [timerExpired, setTimerExpired] = useState(false);
+
+  function handleStart() {
+     timer.current = setTimeout(() => {
+      setTimerExpired(true);
+    }, targetTime * 1000);
+
+    setTimerStarted(true);
+  }
+
+function handleStop() {
+  clearTimeout(timer.current);
+}
+
   return (
     <section className="challenge">
       <h2>{title}</h2>
+      {timerExpired && <p>You lost!</p>}
       <p className="challenge-time">
         {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
       <p>
-        <button> Start challenge</button>
+        <button onClick={timerStarted ? handleStop : handleStart}>
+          {timerStarted ? "Stop" : "Start"} Challenge
+        </button>
       </p>
-      <p className="">
-        Time is running...  / Timer inactive
+      <p className={timerStarted ? "active" : undefined}>
+        {timerStarted ? 'Time is running...' : 'Timer inactive'}
       </p>
     </section>
   );
