42f8e8edaae230c3917e23002f053d0991857837
anouar garbaa <g.anouar@yahoo.com>
2024-10-03 09:26:19 +0100

    add the success & lose msg

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,20 +1,38 @@
-import {forwardRef} from 'react';
+import { forwardRef, useImperativeHandle, useRef } from "react";
 
-const ResultModel = forwardRef(function ResultModel({result, targetTime}, ref) {
+const ResultModel = forwardRef(function ResultModel(
+  { targetTime, remainingTime, onReset },
+  ref
+) {
+  // we use useImperativeHandle to define properties and methods that should be accessible on this component from outside
+
+  const dialog = useRef();
+
+  const userLost = remainingTime <= 0 ;
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showModal();
+      }
+    }
+  });
+
   return (
-    <dialog ref={ref} className="result-modal" >
-      <h2>You {result}</h2>
+    <dialog ref={dialog} className="result-modal">
+      {userLost && <h2>You lost</h2>}
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
       </p>
       <p>
-        You stopped the timer with <strong>X seconds left.</strong>
+        You stopped the timer with <strong>{formattedRemainingTime}</strong>
       </p>
-      <form method="dialog">
+      <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
     </dialog>
   );
-})
+});
 
 export default ResultModel;
